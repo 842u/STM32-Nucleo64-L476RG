a96a162e8a424d16c358d82d33cfd27576fdf820
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-01-14 17:47:02 +0100

    refactor: cleanup main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,4 @@
 #include "main.h"
-#include "stm32l4xx_hal.h"
-
-#define ON_BOARD_LD2_BLINK_DELAY_BASE 50
-#define ON_BOARD_LD2_PIN GPIO_PIN_5
-#define ON_BOARD_LD2_PORT GPIOA
-
-#define ON_BOARD_B1_PIN GPIO_PIN_13
-#define ON_BOARD_B1_PORT GPIOC
-#define ON_BOARD_B1_DEBOUNCE_DELAY 10
 
 int main() {
   HAL_Init();
