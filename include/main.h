a1a2ce566934fd0b91008a95505efff9f7514a2a
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-01-14 17:45:15 +0100

    feat: handle B1 to change LD2 delay

--- include/main.h
+++ include/main.h
@@ -1,4 +1,7 @@
 #include "stm32l4xx_hal.h"
 
 void setupOnBoardLD2(GPIO_InitTypeDef *GPIOInitStruct);
-void blinkOnBoardLD2(void);
+void blinkOnBoardLD2(int delay);
+
+void setupOnBardB1(GPIO_InitTypeDef *GPIOInitStruct);
+void handleOnBoardB1(int *delayMultiplier);
--- src/main.c
+++ src/main.c
@@ -1,19 +1,30 @@
 #include "main.h"
 #include "stm32l4xx_hal.h"
 
+#define ON_BOARD_LD2_BLINK_DELAY_BASE 50
 #define ON_BOARD_LD2_PIN GPIO_PIN_5
 #define ON_BOARD_LD2_PORT GPIOA
-#define ON_BOARD_LD2_BLINK_DELAY 250
+
+#define ON_BOARD_B1_PIN GPIO_PIN_13
+#define ON_BOARD_B1_PORT GPIOC
+#define ON_BOARD_B1_DEBOUNCE_DELAY 10
 
 int main() {
   HAL_Init();
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
 
-  GPIO_InitTypeDef GPIOInitStruct = {0};
-  setupOnBoardLD2(&GPIOInitStruct);
+  GPIO_InitTypeDef LD2InitStruct = {0};
+  setupOnBoardLD2(&LD2InitStruct);
+
+  GPIO_InitTypeDef B1InitStruct = {0};
+  setupOnBardB1(&B1InitStruct);
+
+  int delayMultiplier = 1;
 
   while (1) {
-    blinkOnBoardLD2();
+    handleOnBoardB1(&delayMultiplier);
+    blinkOnBoardLD2(ON_BOARD_LD2_BLINK_DELAY_BASE * delayMultiplier);
   }
 }
 
@@ -27,7 +38,33 @@ void setupOnBoardLD2(GPIO_InitTypeDef *GPIOInitStruct) {
   HAL_GPIO_Init(ON_BOARD_LD2_PORT, GPIOInitStruct);
 }
 
-void blinkOnBoardLD2(void) {
+void blinkOnBoardLD2(int delay) {
   HAL_GPIO_TogglePin(ON_BOARD_LD2_PORT, ON_BOARD_LD2_PIN);
-  HAL_Delay(ON_BOARD_LD2_BLINK_DELAY);
+  HAL_Delay(delay);
+}
+
+void setupOnBardB1(GPIO_InitTypeDef *GPIOInitStruct) {
+  GPIOInitStruct->Pin = ON_BOARD_B1_PIN;
+  GPIOInitStruct->Mode = GPIO_MODE_INPUT;
+  GPIOInitStruct->Pull = GPIO_PULLUP;
+  GPIOInitStruct->Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(ON_BOARD_B1_PORT, GPIOInitStruct);
+}
+
+void handleOnBoardB1(int *delayMultiplier) {
+  if (HAL_GPIO_ReadPin(ON_BOARD_B1_PORT, ON_BOARD_B1_PIN) == GPIO_PIN_RESET) {
+    HAL_Delay(ON_BOARD_B1_DEBOUNCE_DELAY);
+
+    while (HAL_GPIO_ReadPin(ON_BOARD_B1_PORT, ON_BOARD_B1_PIN) ==
+           GPIO_PIN_RESET)
+      ;
+
+    if (*delayMultiplier > 5) {
+      *delayMultiplier = 1;
+    } else {
+      *delayMultiplier *= 2;
+    }
+
+    HAL_Delay(ON_BOARD_B1_DEBOUNCE_DELAY);
+  }
 }
