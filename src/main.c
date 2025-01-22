4b44ca2183573c1e41eaa0b39d8609cb7a763420
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-01-22 15:08:58 +0100

    feat: configure SW1 as interrupt

--- src/main.c
+++ src/main.c
@@ -1,11 +1,31 @@
 #include "main.h"
 #include "pwmTimerRGBLED.h"
 
+#define SW1_PORT GPIOC
+#define SW1_PIN GPIO_PIN_5
+#define SW1_IRQn EXTI9_5_IRQn
+
+void SysTick_Handler(void) { HAL_IncTick(); }
+
+volatile int interruptFlag = 0;
+void EXTI9_5_IRQHandler(void) {
+  if (__HAL_GPIO_EXTI_GET_IT(SW1_PIN) != RESET) {
+    __HAL_GPIO_EXTI_CLEAR_IT(SW1_PIN);
+    interruptFlag = 1;
+  }
+}
+
 int main() {
   HAL_Init();
   pwmTimerRGBLEDInit();
 
-  setD1Rainbow(100);
-}
+  GPIO_InitTypeDef gpioSW1Config = {0};
+  gpioSW1Config.Mode = GPIO_MODE_IT_FALLING;
+  gpioSW1Config.Pin = SW1_PIN;
+  gpioSW1Config.Pull = GPIO_PULLUP;
+  gpioSW1Config.Speed = GPIO_SPEED_LOW;
+  HAL_GPIO_Init(SW1_PORT, &gpioSW1Config);
 
-void SysTick_Handler(void) { HAL_IncTick(); }
+  HAL_NVIC_SetPriority(SW1_IRQn, 2, 0);
+  HAL_NVIC_EnableIRQ(SW1_IRQn);
+}
