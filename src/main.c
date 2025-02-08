d9d06ea524e0c6085dac346e08d5754646ce5feb
Kamil Bażanow <kamil.bazanow@gmail.com>
2025-02-08 14:57:55 +0100

    feat: setup GPIO for UART4_TX

--- src/main.c
+++ src/main.c
@@ -5,6 +5,16 @@ int main() {
   HAL_Init();
   onBoardLD2Init();
 
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+
+  GPIO_InitTypeDef GPIO_UART4_TX_initStruct = {0};
+  GPIO_UART4_TX_initStruct.Pin = GPIO_PIN_10;
+  GPIO_UART4_TX_initStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_UART4_TX_initStruct.Pull = GPIO_NOPULL;
+  GPIO_UART4_TX_initStruct.Alternate = GPIO_AF8_UART4;
+  GPIO_UART4_TX_initStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  HAL_GPIO_Init(GPIOC, &GPIO_UART4_TX_initStruct);
+
   while (1) {
     onBoardLD2Blink(500);
   }
